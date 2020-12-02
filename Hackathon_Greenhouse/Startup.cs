93b46c756ff3387935592997ca5d01d78da27cf5
Akhilesh Boodhun <akhilesh.boodhun@umail.uom.ac.mu>
2020-12-02 07:44:52 +0400

    fix cors

--- Hackathon_Greenhouse/Startup.cs
+++ Hackathon_Greenhouse/Startup.cs
@@ -42,7 +42,6 @@ namespace Hackathon_Greenhouse
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseCors("MyPolicy");
 
             if (env.IsDevelopment())
             {
@@ -58,7 +57,7 @@ namespace Hackathon_Greenhouse
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            app.UseCors("MyPolicy");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
